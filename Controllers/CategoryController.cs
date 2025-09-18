48054c009e50aad84f8f0304c94d718bf2b43120
User <User@WIN10-KERAS-VIS>
2025-09-18 16:44:41 +0300

    add fontawesome

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -28,10 +28,82 @@ namespace WebAppMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);   
+        }
+
+        //GET - для EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null) 
+            { 
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - для EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);   
+        }
+
+
+        //GET - для DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null) 
+            { 
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - для DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+            
+            return View();   
         }
+
+
 
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -7,8 +7,16 @@ namespace WebAppMVC.Models
     {
         [Key]
         public int Id { get; set; }
+        
+
+        [Required(ErrorMessage = "Category Name должно быть заполнено")]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-        [DisplayName("Display Order:")]
+        
+
+        [Required(ErrorMessage = "Display Order должно быть заполнено")]
+        [Range(1,int.MaxValue, ErrorMessage = "Значение Display Order должно быть больше 0")] 
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
