be833aba8d21825ce7cbb7e1b8d845cd31124f6e
User <User@WIN10-KERAS-VIS>
2025-10-20 10:40:21 +0300

    #

--- WEBAPP_Utility/BrainTree/BrainTreeSettings.cs
+++ WEBAPP_Utility/BrainTree/BrainTreeSettings.cs
@@ -8,9 +8,9 @@ namespace WebApp_Utility.BrainTree
 {
     public class BrainTreeSettings
     {
-        public string Environment { get; set; }
-        public string MerchantId { get; set; }
-        public string PublicKey { get; set; }
-        public string PrivateKey { get; set; }
+        public string? Environment { get; set; }
+        public string? MerchantId { get; set; }
+        public string? PublicKey { get; set; }
+        public string? PrivateKey { get; set; }
     }
 }
--- WebApp_DataAccess/obj/Debug/net9.0/WebApp_DataAccess.AssemblyInfo.cs
+++ WebApp_DataAccess/obj/Debug/net9.0/WebApp_DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp_DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e3f00f3881449eaba25450370c5470f50c8244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2865c3c714542c62a6a8bb2f3c6c1532c00d11e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp_DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp_DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
